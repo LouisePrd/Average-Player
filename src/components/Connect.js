4f9a746d8770859485dcf789858de8c890f084fd
LouisePrd <louise.peridy22@gmail.com>
2025-02-24 14:43:22 +0100

    ✏️ Fix typo connect

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -12,7 +12,7 @@ export function Connect() {
     <div className="register">
       <p>But first, what's your pseudo?</p>
       <input type="text" placeholder="Enter your pseudo" />
-      <button onClick={connected}>Let's go</button>
+      <button onClick={connected}>Start</button>
     </div>
   );
 }
