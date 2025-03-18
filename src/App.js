e42a9380a8853263297a26bf697aaec484424777
LouisePrd <louise.peridy22@gmail.com>
2025-03-18 22:01:22 +0100

    ✨ Add Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 import { Champions } from "./pages/Champions";
 import { Home } from "./pages/Home";
 import { Scoreboard } from "./pages/Scoreboard";
 import { Games } from "./pages/Games";
-import { Guesser } from "./pages/games/Guesser";
+import { Guesser } from "./pages/Guesser";
 import { ChampionDetail } from "./pages/ChampionDetails";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/champions" element={<Champions />} />
@@ -21,6 +22,7 @@ function App() {
         <Route path="/games/guesser" element={<Guesser />} />
         <Route path="/champions/:name" element={<ChampionDetail />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
