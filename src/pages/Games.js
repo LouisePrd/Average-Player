c4326f621b62cba718929e29d62737961b57a934
LouisePrd <louise.peridy22@gmail.com>
2025-03-24 17:16:31 +0100

    💄 New game text

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -4,22 +4,28 @@ export function Games() {
   return (
     <div className="games">
       <h1>Games</h1>
-      <p>2 different games, 2 different champions, 2 different outcomes.</p>
+      <p>2 different games, 2 different outcomes.</p>
 
       <div className="guesser">
-        <h2>Guess the Champion with LeBlanc !</h2>
-        <p>Can you guess the champion by their abilities?</p>
-        <button id="btn-games" onClick={() =>  window.location.href = "/games/guesser"}>Play</button>
+        <h2>Guess the Champion</h2>
+        <p>Can you guess the champion by their abilities?<br></br>Try it out!
+
+        </p>
+        <button
+          id="btn-games"
+          onClick={() => (window.location.href = "/games/guesser")}
+        >
+          Play
+        </button>
       </div>
 
-      <div className="memory">
-        <h2>Memory Game with Ekko !</h2>
-        <p>Match the champions to win!</p>
+      <div className="smash-or-pass">
+        <h2>Smash or pass</h2>
+        <p>
+          Would you smash or pass this champion?<br></br>(Please not minors)
+        </p>
         <button id="btn-games">Play</button>
       </div>
-      
-
-
     </div>
   );
 }
